91ca5116afb285c7254161ca065adb0340f17a0b
klevun <niktia28@gmail.com>
2022-05-22 16:50:12 +0300

    Constraints and style were set

--- Navigation/Profile/Photo/PhotosTableViewCell.swift
+++ Navigation/Profile/Photo/PhotosTableViewCell.swift
@@ -12,42 +12,62 @@ class PhotosTableViewCell: UITableViewCell {
     private lazy var photoLabel: UILabel = {
         let label = UILabel()
         label.text = "Мои Фото"
+        label.tintColor = .black
+        label.font = .systemFont(ofSize: 24, weight: .bold)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
 
     private lazy var arrowImage: UIImageView = {
         let image = UIImageView(image: UIImage(systemName: "arrow.right"))
+        image.tintColor = .black
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
 
     private lazy var firstImage: UIImageView = {
         let image = UIImageView(image: UIImage(named: "1"))
+        image.clipsToBounds = true
         image.layer.cornerRadius = 6
+        image.contentMode = .scaleAspectFill
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
 
     private lazy var secondImage: UIImageView = {
         let image = UIImageView(image: UIImage(named: "2"))
+        image.clipsToBounds = true
         image.layer.cornerRadius = 6
+        image.contentMode = .scaleAspectFill
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
 
     private lazy var thirdImage: UIImageView = {
-        let image = UIImageView(image: UIImage(named: "3"))
+        let image = UIImageView(image: UIImage(named: "5"))
+        image.clipsToBounds = true
         image.layer.cornerRadius = 6
+        image.contentMode = .scaleAspectFill
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
 
     private lazy var fourthImage: UIImageView = {
-        let image = UIImageView(image: UIImage(named: "4"))
+        let image = UIImageView(image: UIImage(named: "6"))
+        image.clipsToBounds = true
         image.layer.cornerRadius = 6
+        image.contentMode = .scaleAspectFill
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
+    }()
+
+    private lazy var imageStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.distribution = .fillEqually
+        stack.spacing = 8
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
     }()
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -62,35 +82,26 @@ class PhotosTableViewCell: UITableViewCell {
     private func layout() {
         contentView.addSubview(photoLabel)
         contentView.addSubview(arrowImage)
-        contentView.addSubview(firstImage)
-        contentView.addSubview(secondImage)
-        contentView.addSubview(thirdImage)
-        contentView.addSubview(fourthImage)
+        contentView.addSubview(imageStack)
+        imageStack.addArrangedSubview(firstImage)
+        imageStack.addArrangedSubview(secondImage)
+        imageStack.addArrangedSubview(thirdImage)
+        imageStack.addArrangedSubview(fourthImage)
 
-        let topLabelConstraint = self.photoLabel.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 12)
-        let leadingLabelConstraint = self.photoLabel.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 12)
+        let topLabelConstraint = self.photoLabel.topAnchor.constraint(equalTo: self.contentView.safeAreaLayoutGuide.topAnchor, constant: 12)
+        let leadingLabelConstraint = self.photoLabel.leadingAnchor.constraint(equalTo: self.contentView.safeAreaLayoutGuide.leadingAnchor, constant: 12)
 
-        let topFirstConstraint = self.firstImage.topAnchor.constraint(equalTo: self.photoLabel.bottomAnchor, constant: 12)
-        let bottomFirstConstraint = self.firstImage.bottomAnchor.constraint(equalTo: self.contentView.safeAreaLayoutGuide.bottomAnchor, constant: 12)
-        let leadingFirstConstraint = self.firstImage.leadingAnchor.constraint(equalTo: self.contentView.safeAreaLayoutGuide.leadingAnchor, constant: 12)
+        let trailingArrowConstraint = self.arrowImage.trailingAnchor.constraint(equalTo: self.contentView.safeAreaLayoutGuide.trailingAnchor, constant: -12)
+        let yCenterArrow = self.arrowImage.centerYAnchor.constraint(equalTo: self.photoLabel.centerYAnchor)
 
-        let topScondConstraint = self.secondImage.topAnchor.constraint(equalTo: self.photoLabel.bottomAnchor, constant: 12)
-        let bottomSecondConstraint = self.secondImage.bottomAnchor.constraint(equalTo: self.contentView.safeAreaLayoutGuide.bottomAnchor, constant: 12)
-        let leadingSecondConstraint = self.secondImage.leadingAnchor.constraint(equalTo: self.firstImage.trailingAnchor, constant: 8)
-
-        let topThirdConstraint = self.thirdImage.topAnchor.constraint(equalTo: self.photoLabel.bottomAnchor, constant: 12)
-        let bottomThridConstraint = self.thirdImage.bottomAnchor.constraint(equalTo: self.contentView.safeAreaLayoutGuide.bottomAnchor, constant: 12)
-        let leadingThirdConstraint = self.thirdImage.leadingAnchor.constraint(equalTo: self.secondImage.trailingAnchor, constant: 8)
-
-        let topForthConstraint = self.fourthImage.topAnchor.constraint(equalTo: self.photoLabel.bottomAnchor, constant: 12)
-        let bottomForthConstraint = self.fourthImage.bottomAnchor.constraint(equalTo: self.contentView.safeAreaLayoutGuide.bottomAnchor, constant: 12)
-        let leadingForthConstraint = self.fourthImage.leadingAnchor.constraint(equalTo: self.thirdImage.trailingAnchor, constant: 8)
-        let trailingForthConstraint = self.fourthImage.trailingAnchor.constraint(equalTo: self.contentView.safeAreaLayoutGuide.trailingAnchor, constant: -12)
+        let topStackConstraint = self.imageStack.topAnchor.constraint(equalTo: self.photoLabel.bottomAnchor, constant: 12)
+        let bottomStackConstraint = self.imageStack.bottomAnchor.constraint(equalTo: self.contentView.safeAreaLayoutGuide.bottomAnchor, constant: 12)
+        let leadingStackConstraint = self.imageStack.leadingAnchor.constraint(equalTo: self.contentView.safeAreaLayoutGuide.leadingAnchor, constant: 8)
+        let trailingStackConstraint = self.imageStack.trailingAnchor.constraint(equalTo: self.contentView.safeAreaLayoutGuide.trailingAnchor, constant: -12)
+        let heightStack = self.fourthImage.heightAnchor.constraint(equalToConstant: (UIScreen.main.bounds.width - (12 * 2 + 12 * 3)) / 4)
 
         NSLayoutConstraint.activate([topLabelConstraint, leadingLabelConstraint,
-                                    topFirstConstraint, leadingFirstConstraint, bottomFirstConstraint,
-                                    topScondConstraint, leadingSecondConstraint, bottomSecondConstraint,
-                                    topThirdConstraint, leadingThirdConstraint, bottomThridConstraint,
-                                    topForthConstraint, leadingForthConstraint, bottomForthConstraint, trailingForthConstraint].compactMap( { $0 } ))
+                                     trailingArrowConstraint, yCenterArrow,
+                                     topStackConstraint, bottomStackConstraint, leadingStackConstraint, trailingStackConstraint, heightStack].compactMap( { $0 } ))
     }
 }
